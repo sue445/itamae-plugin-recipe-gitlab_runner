28b8d6af695e8fac2bf48e1f2475b4e98dfe0ea3
sue445 <sue445@sue445.net>
2018-10-07 00:29:26 +0900

    bundle gem itamae-plugin-recipe-gitlab_runner --mit

---
+++ lib/itamae/plugin/recipe/gitlab_runner.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/gitlab_runner/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module GitlabRunner
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ spec/itamae/plugin/recipe/gitlab_runner_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Itamae::Plugin::Recipe::GitlabRunner do
+  it "has a version number" do
+    expect(Itamae::Plugin::Recipe::GitlabRunner::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
