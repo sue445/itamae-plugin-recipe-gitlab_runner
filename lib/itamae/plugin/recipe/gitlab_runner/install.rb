bf2c5dc38d284eeb442ca61f110e26509918e6bc
sue445 <sue445@sue445.net>
2018-10-07 03:15:50 +0900

    Refactor recipe

--- lib/itamae/plugin/recipe/gitlab_runner/install.rb
+++ lib/itamae/plugin/recipe/gitlab_runner/install.rb
@@ -1,7 +1,28 @@
+# c.f. https://docs.gitlab.com/runner/install/linux-repository.html
+
+package "curl"
+
 case node[:platform]
 when "debian"
-  include_recipe "gitlab_runner::install::deb"
+  execute "curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | bash" do
+    not_if "ls /etc/apt/sources.list.d/runner_gitlab-runner.list"
+  end
 
 when "redhat", "amazon"
-  include_recipe "gitlab_runner::install::rpm"
+  execute "curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.rpm.sh | bash" do
+    not_if "ls /etc/yum.repos.d/runner_gitlab-runner.repo"
+  end
+end
+
+gitlab_runner_version =
+  if node["gitlab-runner"]
+    node["gitlab-runner"][:version]
+  else
+    nil
+  end
+
+package "gitlab-runner" do
+  if gitlab_runner_version
+    version gitlab_runner_version
+  end
 end
